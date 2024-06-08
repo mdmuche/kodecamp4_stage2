392153e3cba88ddabeaf2f3010d9b36ebc2f3b52
mdmuche <abulamartins@gmail.com>
2024-06-08 14:09:43 +0100

    modified removeDuplicate

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,5 +1,19 @@
 function removeDuplicate(arr) {
-  return new Set(arr);
+  const uniqueArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    //? either of both cases work
+    // case 1
+    // if (!uniqueArray.includes(arr[i])) {
+    //   uniqueArray.push(arr[i]);
+    // }
+
+    // case 2
+    if (arr[i] !== arr[i + 1]) {
+      uniqueArray.push(arr[i]);
+    }
+  }
+
+  return uniqueArray;
 }
 
 console.log(removeDuplicate([1, 2, 2, 3, 3, 3, 4, 5, 5]));
